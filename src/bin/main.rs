45af097737337d1d62756925e44c3a9789e01aba
Donald Whyte <donaldwhyte0@gmail.com>
2018-09-02 10:14:42 +0200

    Fix bugs in Block hashing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,20 @@
+extern crate chrono;
 extern crate donchain;
 
-use donchain::block;
+use donchain::block::{Block, HASH_SIZE};
+
 
 fn main() {
-    println!("Hello, world!");
+    let dt = chrono::NaiveDateTime::new(
+        chrono::NaiveDate::from_ymd(2018, 9, 2),
+        chrono::NaiveTime::from_hms(7, 36, 55));
+
+    let block = Block::from_slice(
+        0,
+        [0; HASH_SIZE],
+        // TODO: update to use safe `TryInto` when it finally becomes stable
+        dt.timestamp() as u64,
+        b"This is the genesis block of donchain.");
+
+    print!("{}", block);
 }
--- src/block.rs
+++ src/block.rs
@@ -1,20 +1,25 @@
 extern crate byteorder;
+extern crate itertools;
 extern crate sha3;
 
+use std::fmt;
 use std::io::Write;
+
 use self::byteorder::{LittleEndian, WriteBytesExt};
+use self::itertools::Itertools;
 use self::sha3::{Digest, Sha3_512};
 
 
-const BLOCK_SIZE: usize = 1024 * 1024 * 4;  // 4MB
-type BlockData = [u8; BLOCK_SIZE];
+pub const BLOCK_SIZE: usize = 1024;// * 1024 * 4;  // 4MB
+pub type BlockData = [u8; BLOCK_SIZE];
 
 
-const HASH_SIZE: usize = 512;
-type Hash = [u8; HASH_SIZE];
+pub const HASH_SIZE: usize = 64;
+pub type Hash = [u8; HASH_SIZE];
 
 
-struct Block {
+#[derive(Clone)]
+pub struct Block {
     index: u64,
     previous_hash: Hash,
     timestamp: u64,
@@ -23,31 +28,14 @@ struct Block {
 }
 
 impl Block {
-    pub fn from_vec(index: u64,
-                    previous_hash: Hash,
-                    timestamp: u64,
-                    data: Vec<u8>) -> Block
+    pub fn from_slice(index: u64,
+                      previous_hash: Hash,
+                      timestamp: u64,
+                      data: &[u8]) -> Block
     {
-        // TODO: can this copy be avoided? maybe, but doesn't matter, since
-        // copy will be needed anyway?
-        assert!(
-            data.len() <= BLOCK_SIZE,
-            "Got {} bytes of data, but maximum data size is {}",
-            data.len(),
-            BLOCK_SIZE);
-        let mut data_buffer: BlockData = [0; BLOCK_SIZE];
-        data_buffer.copy_from_slice(data.as_slice());
-
-        let mut bytes_to_hash = vec![];
-        bytes_to_hash.write_u64::<LittleEndian>(index).unwrap();
-        bytes_to_hash.write(&previous_hash).unwrap();
-        bytes_to_hash.write_u64::<LittleEndian>(timestamp).unwrap();
-        bytes_to_hash.write(&data_buffer).unwrap();
-
-        let mut hash: Hash = [0; HASH_SIZE];
-        hash_sha3_512(bytes_to_hash.as_slice(), &mut hash);
-
-        Block::new(index, previous_hash, timestamp, data_buffer, hash)
+        let block_data = block_data_from_slice(data);
+        let hash = compute_hash(index, previous_hash, timestamp, block_data);
+        Block::new(index, previous_hash, timestamp, block_data, hash)
     }
 
     pub fn new(index: u64,
@@ -66,9 +54,50 @@ impl Block {
     }
 }
 
+impl fmt::Display for Block {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Block(\nindex={}\ntimestamp={}\nprevious_hash={:02x}\ndata={:02x}\nhash={:02x}\n)",
+            self.index,
+            self.timestamp,
+            self.previous_hash.iter().format(""),
+            self.data.iter().format(""),
+            self.hash.iter().format(""))
+    }
+}
 
-fn hash_sha3_512(data: &[u8], output: &mut Hash) {
+
+pub fn block_data_from_slice(data: &[u8]) -> BlockData {
+    assert!(
+        data.len() <= BLOCK_SIZE,
+        "Got {} bytes of data, but maximum allowed bytes is {}",
+        data.len(),
+        BLOCK_SIZE);
+    let mut block_data: BlockData = [0; BLOCK_SIZE];
+    for i in 0..data.len() {
+        block_data[i] = data[i];
+    }
+    block_data
+}
+
+
+pub fn compute_block_hash(block: Block) -> Hash {
+    compute_hash(block.index, block.previous_hash, block.timestamp, block.data)
+}
+
+
+fn compute_hash(index: u64, previous_hash: Hash, timestamp: u64, data: BlockData) -> Hash {
+    let mut bytes_to_hash = vec![];
+    bytes_to_hash.write_u64::<LittleEndian>(index).unwrap();
+    bytes_to_hash.write(&previous_hash).unwrap();
+    bytes_to_hash.write_u64::<LittleEndian>(timestamp).unwrap();
+    bytes_to_hash.write(&data).unwrap();
+
     let mut hasher = Sha3_512::default();
-    hasher.input(data);
-    output.clone_from_slice(hasher.result().as_slice());
+    hasher.input(bytes_to_hash.as_slice());
+
+    let mut hash: Hash = [0; HASH_SIZE];
+    hash.copy_from_slice(hasher.result().as_slice());
+    hash
 }
